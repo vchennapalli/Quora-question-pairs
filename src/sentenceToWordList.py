4237fee5f2c5014fb4cf8afd0518ea83284b8a8a
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2018-03-08 01:42:21 +0000

    Code for siamese network

---
+++ src/sentenceToWordList.py
@@ -0,0 +1,58 @@
+import re
+import pandas as pd
+
+#FILEPATHS
+TRAIN_CSV = "~/raw_data/train.csv"
+TEST_CSV = "~/raw_data/test.csv"
+COMPUTE_DATA_PATH = "../computed_data/"
+
+#LOADS TRAINING AND TEST SET
+train_df = pd.read_csv(TRAIN_CSV)
+test_df = pd.read_csv(TEST_CSV)
+vocab_size = 121326 #150
+question_cols = ['question1', 'question2']
+
+
+"""preprocesses and converts question to a list of words"""
+def question_to_wordlist(text):
+    """
+    input: string of text
+    output: list of words
+    """
+    text = str(text)
+    text = text.lower()
+
+    #text cleaning and substitutions
+    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+    text = re.sub(r"what's", "what is ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r"\'ve", " have ", text)
+    text = re.sub(r"can't", "cannot ", text)
+    text = re.sub(r"n't", " not ", text)
+    text = re.sub(r"i'm", "i am ", text)
+    text = re.sub(r"\'re", " are ", text)
+    text = re.sub(r"\'d", " would ", text)
+    text = re.sub(r"\'ll", " will ", text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r" e g ", " eg ", text)
+    text = re.sub(r" b g ", " bg ", text)
+    text = re.sub(r" u s ", " american ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+    text = re.sub(r"j k", "jk", text)
+    text = re.sub(r"\s{2,}", " ", text)
+
+    text = text.split()
+
+    return text
---
+++ src/siameseNetwork.py
@@ -0,0 +1,100 @@
+from keras.models import Model
+from keras.layers import Input, Dense, Reshape, merge
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.sequence import skipgrams
+from nltk.corpus import stopwords
+from keras.preprocessing import sequence
+
+import urllib
+import os
+
+import numpy as np
+import tensorflow as tf
+import word2vec
+from sentenceToWordList import *
+
+inverse_dictionary = np.loadtxt(COMPUTE_DATA_PATH + 'inverse_dictionary.npy')
+dictionary = {}
+maxSeqLength = 0
+
+for index in range(len(inverse_dictionary)):
+	dictionary[inverse_dictionary[index]] = index
+	
+for dataTuple in [train_df, test_df]:
+	for index, row in dataTuple.iterrows():
+		for question in question_cols:
+			numVector = []
+			for word in question_to_wordlist(row[question]):
+				if (word in dictionary):
+					numVector.append(dictionary[word])
+			dataTuple.set(index, question, numVector)
+			maxSeqLength = max(maxSeqLength, len(numVector))
+
+validation_size = 40000
+xTrain, xValidation, yTrain, yValidation = train_test_split(train_df[questions_cols], train_df['is_duplicate'], test_size=validation_size)
+for dataTuple in [xTrain, xTest]:
+	for question in questions_cols:
+		dataTuple[question] = pad_sequences(dataset[question], maxLen=maxSeqLength)
+
+
+
+# Model variables
+n_hidden = 50
+gradientClippingNorm = 1.25
+batch_size = 64
+#keep n_epoch a multiple of 5
+n_epoch = 50
+embedding_dim = 300
+embeddingsMatrix = np.loadtxt(COMPUTE_DATA_PATH + 'embedding_matrix.txt', fmt = "%.5f")
+
+
+def exponent_neg_manhattan_distance(left, right):
+    return K.exp(-K.sum(K.abs(left-right), axis=1, keepdims=True))
+
+leftInput = Input(shape=(maxSeqLength,), dtype='int32')
+rightInput = Input(shape=(maxSeqLength,), dtype='int32')
+
+embeddingLayer = Embedding(len(embeddings), embedding_dim, weights=[embeddings], input_length=maxSeqLength, trainable=False)
+
+encodedLeft = embedding_layer(leftInput)
+encodedRight = embedding_layer(rightInput)
+
+sharedLstm = LSTM(n_hidden)
+
+leftOutput = sharedLstm(encodedLeft)
+rightOutput = sharedLstm(encodedRight)
+distance = keras.layers.add()
+
+output = keras.layers.concatenate([leftOutput, rightOutput])
+output = Dense(1, activation = 'sigmoid')(output)
+#malstmDistance = Merge(mode=lambda x: exponent_neg_manhattan_distance(x[0], x[1]), output_shape=lambda x: (x[0][0], 1))([leftOutput, rightOutput])
+
+siameseLSTM = Model([leftInput, rightInput], [output])
+optimizer = Adadelta(clipnorm=gradientClippingNorm)
+
+siameseLSTM.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+
+training_start_time = time()
+
+for i in range(n_epoch/5):
+	siameseLSTMTrained = siameseLSTM.fit([xTrain['question1'], xTrain['question2']], yTrain.values, batch_size=batch_size, nb_epoch=n_epoch,
+                            	validation_data=([xValidation['question1'], xValidation['question2']], yValidation.values))
+	siameseLSTMTrained.save('siameseLSTMTrained.h5')
+
+print("Training time finished.\n{} epochs in {}".format(n_epoch, datetime.timedelta(seconds=time()-training_start_time)))
+
+plt.plot(siameseLSTMTrained.history['acc'])
+plt.plot(siameseLSTMTrained.history['val_acc'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+plt.plot(siameseLSTMTrained.history['loss'])
+plt.plot(siameseLSTMTrained.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper right')
+plt.show() 
