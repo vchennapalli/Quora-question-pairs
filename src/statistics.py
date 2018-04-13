1c70af92274912fe656ec24af7f55b8b00f63215
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2018-04-13 14:32:17 +0000

    Computing statistics for question length Excluding google stopwords 35.0 36.0 40.0 46.0 49.0 51.0 52.0 54.0 56.0 60.0 maxSeqLEngth 105 Excluding stopwords from nltk for questions with length greater than 50 34.0 35.0 38.0 42.0 44.0 46.0 47.0 48.0 49.0 50.0

---
+++ src/statistics.py
@@ -0,0 +1,58 @@
+import itertools
+
+from time import time
+from nltk.corpus import stopwords
+
+
+import numpy as np
+from sentenceToWordList import *
+
+#del test_df
+stops = set(stopwords.words('english'))
+maxSeqLength = 0
+
+lengths = []
+redLen = []
+for dataTuple in [train_df, test_df]:
+	for index, row in dataTuple.iterrows():
+		for question in question_cols:
+			numVector = 0
+			for word in question_to_wordlist(row[question]):
+				if (word not in googleStopWords):
+					numVector = numVector + 1
+			lengths.append(numVector)
+			if (numVector > 50):
+				numVector = 0
+				for word in question_to_wordlist(row[question]):
+					if (word not in stops):
+						numVector = numVector + 1
+			redLen.append(numVector)
+			if (maxSeqLength < numVector):
+				maxSeqLength = numVector
+
+lengths = np.array(lengths)
+print(np.percentile(lengths, 99.6))
+print(np.percentile(lengths, 99.7))
+print(np.percentile(lengths, 99.8))
+print(np.percentile(lengths, 99.9))
+print(np.percentile(lengths, 99.93))
+print(np.percentile(lengths, 99.95))
+print(np.percentile(lengths, 99.96))
+print(np.percentile(lengths, 99.97))
+print(np.percentile(lengths, 99.98))
+print(np.percentile(lengths, 99.99))
+
+print("maxSeqLEngth")
+print(maxSeqLength)
+
+
+print(np.percentile(redLen, 99.6))
+print(np.percentile(redLen, 99.7))
+print(np.percentile(redLen, 99.8))
+print(np.percentile(redLen, 99.9))
+print(np.percentile(redLen, 99.93))
+print(np.percentile(redLen, 99.95))
+print(np.percentile(redLen, 99.96))
+print(np.percentile(redLen, 99.97))
+print(np.percentile(redLen, 99.98))
+print(np.percentile(redLen, 99.99))
