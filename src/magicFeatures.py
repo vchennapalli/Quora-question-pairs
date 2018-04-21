691ec7d5b1f442d05b9be6d8d58573c7fd02f103
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2018-04-21 03:14:35 +0000

    Added more features

--- src/magicFeatures.py
+++ src/magicFeatures.py
@@ -25,16 +25,35 @@ for dataTuple in [train_df, test_df]:
 			nodeIds.append(tmpNodeId)
 		edges[nodeIds[0]].add(nodeIds[1])
 		edges[nodeIds[1]].add(nodeIds[0])
-		
+		if (index%10000 == 0):
+			print(index)
 
 for dataTuple in [train_df, test_df]:
+	dataTuple['q_len1'] = 0
+	dataTuple['q_len2'] = 0
+	dataTuple['diff_len'] = 0
+	dataTuple['word_len1'] = 0
+	dataTuple['word_len2'] = 0
+	dataTuple['common_words'] = 0
+			
 	for index, row in dataTuple.iterrows():
 		a = nodes[row['question1']]
 		b = nodes[row['question2']]
 		dataTuple.set_value(index, 'qid1', a)
 		dataTuple.set_value(index, 'qid2', b)
+		
 		dataTuple.set_value(index, 'min_freq', min(freq[a], freq[b]))
 		dataTuple.set_value(index, 'common_neighbours', len(edges[a].intersection(edges[b])))
+		dataTuple.set_value(index, 'q_len1', len(str(row['question1'])))
+		dataTuple.set_value(index, 'q_len2', len(str(row['question2'])))
+		dataTuple.set_value(index, 'diff_len', len(str(row['question2'])) - len(str(row['question1'])))
+		dataTuple.set_value(index, 'word_len1', len(str(row['question1']).split()))
+		dataTuple.set_value(index, 'word_len2', len(str(row['question2']).split()))
+		tmp = len(set(str(row['question1']).split()).intersection(str(row['question2']).split()))
+		dataTuple.set_value(index, 'common_words', (tmp))
+		if (index%10000 == 0):
+			print(index)
 
+ 
 train_df.to_csv(TRAIN_CSV,sep=',',index=False)
 test_df.to_csv(TEST_CSV,sep=',',index=False)
